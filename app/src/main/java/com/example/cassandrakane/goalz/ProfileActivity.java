a66d2c53066dfa191d37925a5d77a8e7768221c7
Cassandra Kane <cassandrakane@fb.com>
2018-07-16 16:04:33 -0700

    add swipe navigation left/right

--- app/src/main/java/com/example/cassandrakane/goalz/ProfileActivity.java
+++ app/src/main/java/com/example/cassandrakane/goalz/ProfileActivity.java
@@ -1,7 +1,8 @@
 package com.example.cassandrakane.goalz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        OnSwipeTouchListener onSwipeTouchListener = new OnSwipeTouchListener(ProfileActivity.this) {
+            @Override
+            public void onSwipeLeft() {
+                Intent i = new Intent(getApplicationContext(), FeedActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+            }
+            @Override
+            public void onSwipeRight() {
+                Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+            }
+        };
+
+        getWindow().getDecorView().getRootView().setOnTouchListener(onSwipeTouchListener);
     }
 }
