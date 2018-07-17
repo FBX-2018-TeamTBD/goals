94186f5444122db6e3114613c7c5fce966ef78b2
Demar Edwards <dedwards@fb.com>
2018-07-17 16:26:56 -0700

    Added methods to Goal model

--- app/src/main/java/com/example/cassandrakane/goalz/models/Goal.java
+++ app/src/main/java/com/example/cassandrakane/goalz/models/Goal.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 public class Goal extends ParseObject implements Parcelable {
 
     private boolean isSelected = false;
-    
+
     public Goal() {
         super();
     }
