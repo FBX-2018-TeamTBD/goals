b97c7e735ec268b04c9e04686ff8df9d59f446be
Carol Wang <carolwang@fb.com>
2018-08-07 10:39:21 -0700

    fixed adding profile pic on signup

--- app/src/main/java/com/example/cassandrakane/goalz/SignupActivity.java
+++ app/src/main/java/com/example/cassandrakane/goalz/SignupActivity.java
@@ -1,14 +1,20 @@
 package com.example.cassandrakane.goalz;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,7 +31,11 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -42,7 +52,7 @@ public class SignupActivity extends AppCompatActivity {
     public final static int GALLERY_IMAGE_ACTIVITY_REQUEST_CODE = 134;
 
     private ParseFile imageFile;
-    private String photoFileName = "profilephoto.jpg";
+    private String photoFileName;
     private File photoFile;
 
     @Override
@@ -130,14 +140,99 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void selectImage(View v) {
-        Util.selectImage(this, photoFileName);
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    onLaunchCamera();
+                } else if (items[item].equals("Choose from Library")) {
+                    onLaunchGallery();
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
-
+    public void onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = Util.getPhotoFileUri(photoFileName, this);
+        // wrap File object into a content provider
+        // required for API >= 24
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.fbu.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        try {
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+                    if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                    }
+                }
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        } catch(SecurityException e) {
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+                    if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                    }
+                }
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        }
+    }
+    public void onLaunchGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        if (Build.VERSION.SDK_INT >= 23) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, GALLERY_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        }
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, GALLERY_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                Util.getImageFromCamera(this, imageFile, ivProfile);
+                String imagePath = photoFile.getAbsolutePath();
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // Compress the image further
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(imagePath);
+                    // Write the bytes of the bitmap to
+                    try {
+                        fos.write(bytes.toByteArray());
+                        fos.close();
+                        imageFile = new ParseFile(new File(imagePath));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e) {
+                    Log.i("asdf", "error");
+                    e.printStackTrace();
+                }
+                ivProfile.setImageDrawable(Util.createRoundedBitmapDrawableWithBorder(getResources(), bitmap, R.color.orange));
             } else { // Result was a failure
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
@@ -151,10 +246,36 @@ public class SignupActivity extends AppCompatActivity {
                         }
                     }
                     Uri uri = data.getData();
-                    Util.getImageFromGallery(this, uri, imageFile, ivProfile);
+                    File file = new File(Util.getPath(this, uri));
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // Configure byte output stream
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    // Compress the image further
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(file.getAbsolutePath());
+                        // Write the bytes of the bitmap to file
+                        try {
+                            fos.write(bytes.toByteArray());
+                            fos.close();
+                            imageFile = new ParseFile(file);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (FileNotFoundException e) {
+                        Log.i("asdf", "error");
+                        e.printStackTrace();
+                    }
+                    ivProfile.setImageDrawable(Util.createRoundedBitmapDrawableWithBorder(getResources(), bitmap, R.color.orange));
+                } else { // Result was a failure
+                    Toast.makeText(this, "Picture wasn't selected!", Toast.LENGTH_SHORT).show();
                 }
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't selected!", Toast.LENGTH_SHORT).show();
             }
         }
     }
